6da4ee9107df1c08656936a3fe8d0b162392b550
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2015-08-08 12:18:04 +0300

    refactor(Scene) added desk

--- iProg3_082015_ArcanoidDemo/GameScene.m
+++ iProg3_082015_ArcanoidDemo/GameScene.m
@@ -8,26 +8,57 @@
 
 #import "GameScene.h"
 
+@interface GameScene ()
+
+@property (nonatomic, strong) SKSpriteNode *ball;
+@property (nonatomic, strong) SKSpriteNode *desk;
+
+@end
+
 @implementation GameScene
 
--(void)didMoveToView:(SKView *)view {
+#pragma mark - Properties Overload
+
+- (SKSpriteNode *)ball
+{
+    if (!_ball){
+        _ball = (SKSpriteNode *)[self childNodeWithName:@"ball"];
+        NSParameterAssert(_ball);
+        _ball.physicsBody.friction       = 0;
+        _ball.physicsBody.restitution    = 1;
+        _ball.physicsBody.linearDamping  = 0;
+        _ball.physicsBody.angularDamping = 0;
+        _ball.physicsBody.allowsRotation = NO;
+    }
+    return _ball;
+}
+
+- (SKSpriteNode *)desk
+{
+    if (!_desk){
+        _desk = (SKSpriteNode *)[self childNodeWithName:@"desk"];
+        NSParameterAssert(_desk);
+        _desk.physicsBody.friction       = 0;
+        _desk.physicsBody.restitution    = 1;
+        _desk.physicsBody.linearDamping  = 0;
+        _desk.physicsBody.angularDamping = 0;
+        _desk.physicsBody.allowsRotation = NO;
+        
+        _desk.physicsBody.dynamic        = NO;
+        NSParameterAssert(_desk);
+    }
+    return _desk;
+}
+
+- (void)didMoveToView:(SKView *)view {
     SKPhysicsBody *border = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     border.friction       = 0;
     self.physicsBody      = border;
     
     self.physicsWorld.gravity = CGVectorMake(0, 0);
     
-    SKSpriteNode *ball = (SKSpriteNode *)[self childNodeWithName:@"ball"];
-    NSParameterAssert(ball);
-    ball.physicsBody.friction       = 0;
-    ball.physicsBody.restitution    = 1;
-    ball.physicsBody.linearDamping  = 0;
-    ball.physicsBody.angularDamping = 0;
-    ball.physicsBody.allowsRotation = NO;
-
-    
-    [ball.physicsBody applyImpulse:CGVectorMake(15, -10)];
-    
+    [self.ball.physicsBody applyImpulse:CGVectorMake(15, -10)];
+    self.desk.color = [UIColor purpleColor];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
